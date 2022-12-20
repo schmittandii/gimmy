6d013c861b506f5cade31d33a584ce692724d6aa
Victor Bello <you@example.com>
2022-12-20 21:20:09 +0100

    first commit

---
+++ components/webfooter.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+type Props = {
+    src: any
+}
+
+export default function WebFooter({src}: Props) {
+  return (
+    <div className="w-full">
+          <Image
+           src={src}
+           alt="screenshot1"
+           height={1200}
+           width={1200}
+          />
+          </div>
+  )
+}
